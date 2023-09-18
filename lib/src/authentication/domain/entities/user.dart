c8d23d87fea1c77f12ba8fe2fa14609539a19baa
Aditya Anand <aditechdevofficial@gmail.com>
2023-09-18 18:53:32 +0530

    :art: User Data

---
+++ lib/src/authentication/domain/entities/user.dart
@@ -0,0 +1,32 @@
+import 'package:equatable/equatable.dart';
+
+class User extends Equatable {
+  const User({
+    required this.id,
+    required this.avatar,
+    required this.createdAt,
+    required this.name,
+  });
+
+  final int id;
+  final String createdAt;
+  final String name;
+  final String avatar;
+
+  @override
+  List<Object?> get props => [id];
+
+  //* Instead of below code use equitable
+  // Dart check refererance equality => It check whether it is checking to the same referance
+  //https://api.dart.dev/stable/3.1.2/dart-core/Object/operator_equals.html#:~:text=The%20equality%20operator.,equality%20relation%20on%20a%20class.
+  //
+  //*/
+  // @override
+  // bool operator ==(other) {
+  //   return identical(this, other) ||
+  //       other is User && other.runtimeType == runtimeType && other.id == id && other.name == name;
+  // }
+
+  // @override
+  // int get hashCode => id.hashCode ^ name.hashCode;
+}
